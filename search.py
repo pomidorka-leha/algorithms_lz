994f39d535a838bce953450df2efa50848d0d6a9
pomidorka-leha <Pomidor2048@tutamail.com>
2025-10-31 19:27:46 +0300

    search.py

--- search.py
+++ search.py
@@ -1,13 +1,31 @@
-import random as rd
-a= [ ]
-b=0
+import random
+l = []
+d = []
+k=0
+while len(l)<10: #заполняем список 100 элементами без дублей
+    a= random.randint(0,100)
+    if a not in l : 
+        l.append(a)
+d=sorted(l)
+print(d)
+n=int(input('Введите искомое число: ')) #запрашиваем ввод числа
+for i in range(len(d)): #выполняем линейный поиск
+    if n == d[i]:
+        print('Количество сравнений в линейном поиске: ',i)
+        print('Индекс искомого элемента: ', i)
+        
+low = 0
+high = len(d) - 1
+mid = len(d) // 2
+while d[mid] != n and low <= high: #выполняем бинарный поиск
+    if n > d[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+    k += 1
+    mid = (low + high) // 2
 
-for i in range(100):
-    a.append(rd.randint(1,1000000))
-a2= sorted(a)    
-li = []
-for i in a2:
-  if i not in li:
-    li.append(i)
-print(li)
-
+if low > high:
+    print('Введенного элемента в списке нет')
+else:
+    print('Количество сравнений в бинарном поиске: ', k)    
